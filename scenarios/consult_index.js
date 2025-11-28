1cf29bfabe1fcc3020a66d4c32a922735c31e0d8
fantadem <fanta.dembele@utt.fr>
2025-11-28 16:32:14 +0100

    Add visit function for page navigation and scrolling

---
+++ scenarios/consult_index.js
@@ -0,0 +1,11 @@
+const visit = async (page) => {
+    await page.goto('', {
+        waitUntil: 'networkidle',
+    });
+    await page.waitForTimeout(10000);
+    await page.scrollToEnd();
+    await page.waitForNetworkIdle();
+    await page.waitForTimeout(7000);
+  };
+  
+  module.exports = visit;
