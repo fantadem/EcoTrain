85bfa488f6fc4563f96d25d23abdae01142c5cbd
Fanta Dembele <fanta.dembele@utt.fr>
2025-12-18 21:23:22 +0100

    update

--- scenarios/consult_results.js
+++ scenarios/consult_results.js
@@ -1,5 +1,5 @@
 const visit = async (page) => {
-    await page.goto('trips?departure=Troyes&arrival=Paris&date=2025-01-01&time=00h&passengers=1', {
+    await page.goto('http://127.0.0.1/trips?departure=Troyes&arrival=Paris&date=2025-01-01&time=00h&passengers=1', {
         waitUntil: 'networkidle',
     });
     await page.waitForTimeout(10000);
